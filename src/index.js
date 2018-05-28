c7561c80446102d7ef04db9ca23e9c7b33d6c3b3
ivanberry <tabvim.dev@gmail.com>
2018-05-28 08:23:03 +0800

    Add api call test with VScode rest plugin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.post("/api/auth", (req, res) => {
+  res.status(400).json({ errors: { global: "Invalid credentials" } });
+});
+
 app.get("/", (req, res) => {
   res.end("Hello, world");
 });
